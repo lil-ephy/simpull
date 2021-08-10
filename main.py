087142c3cbc03f8256565e202d1c6d4de4d69d84
elliewhatever <me@elliewhatever.com>
2021-08-11 00:52:44 +0100

    Added approaches to url grab

--- main.py
+++ main.py
@@ -6,7 +6,25 @@ def get_api_key(filename='key'):
         return key.read()
 
 
-class nameOfClass():
+def get_url(filename='url'):
+    with open(filename) as url:
+        return list(x.strip() for x in url.readlines())
+
+
+def get_url2(filename='url'):
+    with open(filename) as url:
+        x = list(x.strip() for x in url.readlines())
+        for each in x:
+            yield each
+
+
+def get_url3(filename='url'):
+    with open(filename) as urlList:
+        urlList = [each for each in [x.strip() for x in urlList.readlines()]]
+        yield urlList
+
+
+class simpull():
 
     def __init__(self):
         self.api_key = get_api_key()
@@ -15,13 +33,23 @@ class nameOfClass():
         return requests.get(url=(f"https://api.similarweb.com/capabilities?"
                                  f"api_key={self.api_key}"))
 
-    def lead_gen(self):
+    def lead_gen(self, start_date='2020-08', end_date='2021-07',
+                 country='world', url=get_url):
         return requests.get(url=(f"https://api.similarweb.com/v1/website/"
                                  f"bbc.co.uk/lead-enrichment/all?api_key="
-                                 f"{self.api_key}&start_date=2019-01&"
-                                 f"end_date=2019-09&country=us&"
+                                 f"{self.api_key}&start_date={start_date}&"
+                                 f"end_date={end_date}&country={country}&"
                                  f"main_domain_only=false&format=json&"
                                  f"show_verified=false"))
 
 
-print(nameOfClass().capabilities())
+print(get_url3())
+
+for i in get_url3():
+    print(i)
+
+# for k, v in enumerate(get_url()):
+#     print(simpull.lead_gen())
+
+# print(get_url())
+# print(nameOfClass().capabilities().text)
