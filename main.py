3a31e356ae7256215d1581ee584ad4c652e61517
elliewhatever <me@elliewhatever.com>
2021-08-11 01:56:01 +0100

    Working bulk downloader

--- main.py
+++ main.py
@@ -1,55 +1,42 @@
 import requests
+import json
 
-
-def get_api_key(filename='key'):
+def get_api_key(filename="key"):
     with open(filename) as key:
         return key.read()
 
 
-def get_url(filename='url'):
-    with open(filename) as url:
-        return list(x.strip() for x in url.readlines())
-
-
-def get_url2(filename='url'):
-    with open(filename) as url:
-        x = list(x.strip() for x in url.readlines())
-        for each in x:
-            yield each
-
-
-def get_url3(filename='url'):
+def get_url(filename="url"):
     with open(filename) as urlList:
-        urlList = [each for each in [x.strip() for x in urlList.readlines()]]
-        yield urlList
+        return [url.strip() for url in urlList.readlines()]
 
 
-class simpull():
-
+class simpull:
     def __init__(self):
         self.api_key = get_api_key()
 
     def capabilities(self):
-        return requests.get(url=(f"https://api.similarweb.com/capabilities?"
-                                 f"api_key={self.api_key}"))
+        return requests.get(
+            url=(f"https://api.similarweb.com/capabilities?" f"api_key={self.api_key}")
+        )
 
-    def lead_gen(self, start_date='2020-08', end_date='2021-07',
-                 country='world', url=get_url):
-        return requests.get(url=(f"https://api.similarweb.com/v1/website/"
-                                 f"bbc.co.uk/lead-enrichment/all?api_key="
-                                 f"{self.api_key}&start_date={start_date}&"
-                                 f"end_date={end_date}&country={country}&"
-                                 f"main_domain_only=false&format=json&"
-                                 f"show_verified=false"))
+    def lead_gen(self, url, start_date="2020-08", end_date="2021-07", country="world"):
+        return requests.get(
+            url=(
+                f"https://api.similarweb.com/v1/website/"
+                f"{url}/lead-enrichment/all?api_key="
+                f"{self.api_key}&start_date={start_date}&"
+                f"end_date={end_date}&country={country}&"
+                f"main_domain_only=false&format=json&"
+                f"show_verified=false"
+            )
+        )
 
 
-print(get_url3())
+print(get_url())
 
-for i in get_url3():
-    print(i)
-
-# for k, v in enumerate(get_url()):
-#     print(simpull.lead_gen())
-
-# print(get_url())
-# print(nameOfClass().capabilities().text)
+# #### WORKING DOWNLOADER #####
+for each in get_url():
+    with open(f'{each}.json', 'w') as outfile:
+        json.dump(simpull().lead_gen(each).json(), outfile, indent=4)
+# #############################
